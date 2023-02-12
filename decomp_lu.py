931e64a185414e41cbb9dfc27961436170761a8f
Marco Tulio A de Barros <marcotuiio@MarcoTuiio.localdomain>
2023-02-12 11:28:07 -0300

    Updated LU

--- decomp_lu.py
+++ decomp_lu.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def decomp_lu(U, L):
+def decomp_lu(U, L, b):
     n = U.shape[0]
     # Decomposição LU
     for j in range(0, n-1):
@@ -10,18 +10,38 @@ def decomp_lu(U, L):
                 U[i, k] = U[i, k] - L[i, j] * U[j, k]  # combinação linear
             U[i, j] = 0
 
-    return U, L
+    # Substituição progressiva
+    Z = np.ones(n)
+    for i in range(0, n):
+        Z[i] = b[i]
+        for j in range(0, i):
+            Z[i] = Z[i] - L[i, j] * Z[j]
+        Z[i] = Z[i] / L[i, i]
 
+    # Retrosubstituição
+    X = np.ones(n)
+    for i in range(n - 1, -1, -1):
+        X[i] = Z[i]
+        for j in range(i + 1, n):
+            X[i] = X[i] - U[i, j] * X[j]
+        X[i] = X[i] / U[i, i]
 
-A = np.asmatrix([[25., 5., 1.],
-                 [64., 8., 1.],
-                 [144., 12., 1.]])
+    return U, L, X
+
+A = np.asmatrix([[1., 1., 1.],
+                 [2., 1., -1.],
+                 [2., -1., 1.]])
 
 I = np.asmatrix([[1., 0., 0.],
                  [0., 1., 0.],
                  [0., 0., 1.]])
+                
+b = np.asmatrix([[-2.],
+                [1.],
+                [3.]])
 
-u, l = decomp_lu(A, I)
+u, l, x = decomp_lu(A, I, b)
 
 print('Matriz U:\n', u)
-print('Matriz L:\n', l)
+print('\nMatriz L:\n', l)
+print('\nVetor solução:\n', x)
