1abef16d14a12bd10063f35f4f54080625ccb554
Marco T Barros <marcotulio.barros@uel.br>
2023-04-27 14:02:56 -0300

    Need to fix quadratura de gauss

--- Numeric_Integration/quadratura_gauss.py
+++ Numeric_Integration/quadratura_gauss.py
@@ -1,9 +1,9 @@
 import numpy as np
 import sympy as sp
 
-def quadr_gauss(a, b, n, f):
+def quadr_gauss(a, b, n, f, X):
     # Definindo as raizes e Ci padrões do intervalo [-1, 1]
-    raizes_padroes = [np.array([-1.3*np.sqrt(3), 1.3*np.sqrt(3)]),
+    raizes_padroes = [np.array([-np.sqrt(3)/3, np.sqrt(3)/3]),
                     np.array([-np.sqrt(15)/5, 0, np.sqrt(15)/5]),
                     np.array([-np.sqrt(525+70*np.sqrt(30))/35, -np.sqrt(525-70*np.sqrt(30))/35, np.sqrt(525-70*np.sqrt(30))/35, np.sqrt(525+70*np.sqrt(30))/35]),
                     np.array([-np.sqrt(245+14*np.sqrt(70))/21, -np.sqrt(245-14*np.sqrt(70))/21, 0, np.sqrt(245-14*np.sqrt(70))/21, np.sqrt(245+14*np.sqrt(70))/21])]
@@ -12,17 +12,19 @@ def quadr_gauss(a, b, n, f):
                 np.array([(18-np.sqrt(30))/36, (18+np.sqrt(30))/36, (18+np.sqrt(30))/36, (18-np.sqrt(30))/36]),
                 np.array([(322-13*np.sqrt(70))/900, (322+13*np.sqrt(70))/900, 128/225, (322+13*np.sqrt(70))/900, (322-13*np.sqrt(70))/900])]
     
-    vet_x = np.zeros(n)
     # 1 calcular os n xi dependendo do intervalo:
+    vet_x = np.zeros(n)
     for i in range(n):
         vet_x[i] = ((b - a)*raizes_padroes[n-2][i] + a + b) / 2
 
+    print('\nPontos xi =', vet_x)
     aprox = 0
-    for i in range(n):
-        aprox = Ci_padroes[n-2][i] * f(vet_x[i]) + aprox
-    
+    for i in range(0, n):
+        aprox = Ci_padroes[n-2][i] * f.subs(X, vet_x[i]) + aprox
+
+    print('\nFunção f(x) =', f)
     aprox = aprox * (b - a) / 2
-    print("Aproximação: ", aprox)
+    print('\nIntegral aproximada para n =', n, 'no intervalo [', a, ',', b, '] =', aprox.round(6), '\n')
 
 X = sp.Symbol('x')
 n = 2
@@ -30,5 +32,4 @@ a = -3
 b = 2
 f = sp.exp(-X)*sp.cos(X)
 
-quadr_gauss(a, b, n, f)
-
+quadr_gauss(a, b, n, f, X)
