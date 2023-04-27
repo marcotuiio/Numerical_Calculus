9cb42e019df98eca608b34db734af590ba8e3898
Marco T Barros <marcotulio.barros@uel.br>
2023-04-27 12:55:01 -0300

    Metodo de integração numerica Newton-Cotes DONE

---
+++ Numeric_Integration/newton_cotes.py
@@ -0,0 +1,35 @@
+import numpy as np
+import sympy as sp
+
+### Altere o valor n, o intervalo [a,b] e a função f(x) que deseja integrar por aproximação
+
+def newton_cotes(f, a, b, n):
+    print("\nMétodo de Newton-Cotes\n")
+    
+    integral_aprox = 0;
+    h = (b - a) / n
+    if n == 1: # regra do trapézio
+        integral_aprox = (h/2) * (f.subs(X, a) + f.subs(X, b))
+    elif n == 2: # regra de Simpson
+        integral_aprox = (h/3) * (f.subs(X, a) + 4*f.subs(X, (a+b)/2) + f.subs(X, b))
+    elif n == 3: # regra de 1/3 de Simpson
+        integral_aprox = (3*h/8) * (f.subs(X, a) + 3*f.subs(X, (2*a+b)/3) + 3*f.subs(X, (a+2*b)/3) + f.subs(X, b))
+    elif n == 4: # regra de newton-cotes generalizada  
+        integral_aprox = (2*h/45) * (7*f.subs(X, a) + 32*f.subs(X, (3*a+b)/4) + 12*f.subs(X, (a+b)/2) + 32*f.subs(X, (a+3*b)/4) + 7*f.subs(X, b))
+    else:
+        print("Não é possível calcular a integral com o número de pontos informado.")
+        return
+
+    print('Função f(x) =', f)
+    print('Aproximação: ', integral_aprox)
+    print('Integral aproximada para n =', n)
+    print('No intervalo [', a, ',', b, '] =')
+    print('---> ', (integral_aprox.evalf()).round(6))
+
+X = sp.Symbol('x')
+n = 1
+a = 0
+b = sp.pi / 4
+f = sp.sin(X)
+
+newton_cotes(f, a, b, n)
